990de87085feb8d8c7a1bcea2fec45de9351d5dd
Wladikgadik <Wlad_95@list.ru>
2017-02-09 00:09:22 +0300

    101

--- main.py
+++ main.py
@@ -7,24 +7,36 @@
 #  
 #  
 import os
+from subprocess import Popen, PIPE
 
 def main(args):
 	path="/home/vladislav/docker/"
 	if not os.path.exists(path):
 		os.mkdir(path)
-		os.chdir(path)
+		
 	
 	f=open(path+"Dockerfile", 'w') #создание файла-инструкции образа Docker
 	
 	############################################
 	##########	Текст Докерфайла	############
 	############################################
-	text="FROM ubuntu "+
-		"MAINTAINER nuttzipper@gmail.com"+
-		"RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends gcc  g++  python  git"+
-		"RUN git clone git://github.com/nuttzipper/ns-3.git"+
-		"RUN cd ns-3-dev-git/; ./waf configure; ./waf build"+
-		"RUN ln -s ns-3-dev-git/waf /ns3"
+	text='''FROM ubuntu
+		MAINTAINER wlad.babkin@mail.com
+		RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+		RUN apt-get update
+		RUN apt-get -y install  --no-install-recommends g++
+		RUN apt-get -y install  --no-install-recommends gcc 
+		RUN apt-get -y install  --no-install-recommends python
+		RUN apt-get -y install git
+		RUN mkdir /ns3
+		RUN cd /ns3
+		RUN git clone git://github.com/Wladikgadik/ns3-not-compiled.git
+		
+		RUN cd /ns3-not-compiled/ns-3.26; ./waf configure; ./waf build'''
+	f.write(text)
+	f.close()
+	#subprocess.call(["docker build "+path])
+	Popen("docker build "+path, shell=True)
 	
 	return 0
 
